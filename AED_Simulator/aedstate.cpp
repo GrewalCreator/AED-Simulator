498e32ef47a266fc3f9250224e7fe10277147f86
ManueLucas <m.lucas2603@gmail.com>
2023-12-02 22:56:12 -0500

    Added more state logic, including setback cas

--- AED_Simulator/aedstate.cpp
+++ AED_Simulator/aedstate.cpp
@@ -1,5 +1,8 @@
 #include "aedstate.h"
 
+#define MIN_NOMINAL_BPM 60
+#define MAX_NOMINAL_BPM 150
+
 AEDState::AEDState(AEDController* c){
     controller = c;
     delay = 0;
@@ -35,7 +38,7 @@ void GetHelpState::stepProgress(){
 
 void PadPlacementState::stepProgress(){
     controller->illuminate(LIGHTUP_PADS);
-    controller->print("Place pads on the patient.");
+    if(controller->getTimeElapsed()<10)controller->print("PLACE PADS ON THE PATIENT.");
     qDebug()<<controller->getPatient()->getHasPadsOn();
     if(controller->getPatient()->getHasPadsOn() && !controller->getPatient()->getImproperPlacement()){//IMPORTANT: if pads are on and they are properly placed. replace this with a proper check function later.
         delay++;
@@ -44,6 +47,7 @@ void PadPlacementState::stepProgress(){
         }
         else{
             delay = 0;
+            controller->resetTimeElapsed();
             controller->setState(ANALYZE_ECG);
         }
     }
@@ -56,22 +60,81 @@ void PadPlacementState::stepProgress(){
 void AnalysisState::stepProgress(){
     delay++;
     controller->illuminate(LIGHTUP_STANDCLEAR);
-    controller->print("PLACEHOLDER MESSAGE FOR ANALYSIS");
+    if(delay <= 10){
+        controller->print("ANALYZING PATIENT... DON'T TOUCH!");
+    }
+    else{
+        if(controller->getPatient()->getHeartRate() > MAX_NOMINAL_BPM){
+            delay = 0;
+            controller->resetTimeElapsed();
+            controller->setState(SHOCK);
+        }
+        else if(controller->getPatient()->getHeartRate() < MIN_NOMINAL_BPM){
+            delay = 0;
+            controller->resetTimeElapsed();
+            controller->setState(CPR);
+        }
+        else{
+
+            controller->print("Patient is nominal.");
+        }
+    }
 
 }
 
 void ShockState::stepProgress(){
-    delay++;
     controller->illuminate(LIGHTUP_SHOCK);
-    controller->print("PLACEHOLDER MESSAGE FOR SHOCK");
+    controller->print("SHOCKABLE RHYTHM DETECTED.");
+    if(controller->getPatient()->getHeartRate() <= MAX_NOMINAL_BPM){
+        delay = 0;
+        controller->setState(ANALYZE_ECG);
+    }
 
+    if(controller->getAED()->getShockDelivered()){
+            if(delay<4){
+                controller->print("3...");
+            }
+            else if(delay<8){
+                controller->print("2...");
+            }
+            else if(delay<12){
+                controller->print("1...");
+            }
+            else if(delay >= 12){
+                controller->decreaseBPM(controller->getAED()->getAmperage());
+                controller->print("Shock Delivered. Begin Compressions.");
+                delay = 0;
+                controller->resetTimeElapsed();
+                controller->setState(CPR);
+            }
+        delay++;
+    }
 }
 
 void CompressionsState::stepProgress(){
-    delay++;
     controller->illuminate(LIGHTUP_COMPRESSIONS);
-    controller->print("PLACEHOLDER MESSAGE FOR CPR");
+    if(controller->getPatient()->getHeartRate() <= MIN_NOMINAL_BPM){
+        controller->print("UNSHOCKABLE RHYTHM DETECTED. SHOCK NOT ADVISED.");
+    }
+    else if(controller->getPatient()->getHeartRate() >= MAX_NOMINAL_BPM){
+        controller->print("SHOCK DELIVERED. STARTING COMPRESSIONS...");
+    }
+    else{//IMPORTANT: if the patient's HR enters the nominal range, do we want to stop compressions? right now, if we get them into this range, then we stop and analyse.
+        delay = 0;
+        controller->resetTimeElapsed();
+        controller->setState(ANALYZE_ECG);
+    }
 
+    if(delay < 120){//2 minutes for compression stage
+        //do compression stuff here
+
+    }
+    else{
+        delay = 0;
+        controller->resetTimeElapsed();
+        controller->setState(ANALYZE_ECG);
+    }
+    delay++;
 }
 
 void NominalState::stepProgress(){
